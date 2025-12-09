ef86c931f82d930c4b9a5864f41a45599d9ae372
Gatomek <tomasz.galinski@gmail.com>
2025-12-09 23:37:57 +0100

    initial commit

--- Listener/src/main/java/pl/gatomek/rabbitmq/demo/listener/ListenerApplication.java
+++ Listener/src/main/java/pl/gatomek/rabbitmq/demo/listener/ListenerApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Profile;
 public class ListenerApplication {
 
     public static final String NUMBER_EXCHANGE = "number-exchange";
+    public static final String EVEN = "even";
+    public static final String ODD = "odd";
 
     public static void main(String[] args) {
         SpringApplication.run(ListenerApplication.class, args);
@@ -28,12 +30,12 @@ public class ListenerApplication {
     @Profile({"odd", "all"})
     @Bean
     public Binding oddBinding(Queue queue, DirectExchange directExchange) {
-        return BindingBuilder.bind(queue).to(directExchange).with("odd");
+        return BindingBuilder.bind(queue).to(directExchange).with(ODD);
     }
 
     @Profile({"even", "all"})
     @Bean
     public Binding evenBinding(Queue queue, DirectExchange directExchange) {
-        return BindingBuilder.bind(queue).to(directExchange).with("even");
+        return BindingBuilder.bind(queue).to(directExchange).with(EVEN);
     }
 }
